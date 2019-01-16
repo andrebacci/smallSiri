5b256234e0083b9aa861217043e332e178558525
Andrea Bacigalupo <bacigalupo.andrea88@gmail.com>
2019-01-16 21:21:32 +0100

    Initial Commit

---
+++ Vocal AssistantTests/Vocal_AssistantTests.swift
@@ -0,0 +1,34 @@
+//
+//  Vocal_AssistantTests.swift
+//  Vocal AssistantTests
+//
+//  Created by Andrea Bacigalupo on 16/01/19.
+//  Copyright © 2019 Andrea Bacigalupo. All rights reserved.
+//
+
+import XCTest
+@testable import Vocal_Assistant
+
+class Vocal_AssistantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
