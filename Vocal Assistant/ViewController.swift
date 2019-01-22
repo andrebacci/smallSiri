42f25567d16c85e7897847f8120579de6d98e64d
andrebacci <bacigalupo.andrea88@gmail.com>
2019-01-22 18:35:57 +0100

    Speech recognize

--- Vocal Assistant/ViewController.swift
+++ Vocal Assistant/ViewController.swift
@@ -10,98 +10,140 @@ import UIKit
 import AVFoundation
 import Speech
 
-class ViewController: UIViewController {
-
-    // private
-    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "it-IT"))!
-    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    private var recognitionTask: SFSpeechRecognitionTask?
-    private let audioEngine = AVAudioEngine()
-    
-    var speechResult = SFSpeechRecognitionResult()
-    
-    let commands:[String] = [
-        "Prova"
-    ]
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     // objects
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var textView: UITextView!
     
+    // private
+    var audioPlayer: AVAudioPlayer!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "it-IT"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
     override func viewDidAppear(_ animated: Bool)
     {
         super.viewDidAppear(animated)
-        
-        SFSpeechRecognizer.requestAuthorization { authStatus in
-            OperationQueue.main.addOperation {
-                var alertTitle = ""
-                var alertMessage = ""
-                
-                switch authStatus {
-                case .authorized:
-                    do {
-                       try self.startRecord()
-                    } catch {
-                        alertTitle = "Recorder Error"
-                        alertMessage = "There was a problem starting the speech recorder"
-                    }
-                    
-                case .denied:
-                    alertTitle = "Speech recognizer not allowed"
-                    alertMessage = "You enable the recgnizer in Settings"
-                    
-                case .restricted, .notDetermined:
-                    alertTitle = "Could not start the speech recognizer"
-                    alertMessage = "Check your internect connection and try again"
-                }
-                
-                
-            }
-        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        speechRecognizer?.delegate = self
     }
     
-    @objc func timerEnded() {
-        // If the audio recording engine is running stop it and remove the SFSpeechRecognitionTask
-        if audioEngine.isRunning {
-            stopRecording()
-            checkForActionPhrases()
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization {authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                print("Authorized")
+                
+                /* if let path = Bundle.main.url(forResource: "test", withExtension:"m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    } */
+                    
+                    /* let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) {(result, error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                            print(result?.bestTranscription.formattedString)
+                        }
+                    } */
+            }
         }
     }
-
-    private func startRecord() throws {
-        if !audioEngine.isRunning {
-            let timer = Timer(timeInterval: 5.0, target: self, selector: #selector(ViewController.timerEnded), userInfo: nil, repeats: false)
-            RunLoop.current.add(timer, forMode: .common)
-            
-            let audioSession = AVAudioSession.sharedInstance()
-            try audioSession.setCategory(AVAudioSession.Category.record
-                , mode: AVAudioSession.Mode.default
-                , options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+    
+    // action
+    @IBAction func recordBtnPress(_ sender: Any) {
+        // request to use speech
+        requestSpeechAuth()
+        
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+        } else {
+            startRecording()
+        }
+    }
+    
+    func startRecording() {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.measurement, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
             try audioSession.setMode(AVAudioSession.Mode.measurement)
-            try audioSession.setActive(true
-                , options: AVAudioSession.SetActiveOptions.notifyOthersOnDeactivation)
-            
-            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-            
-            let inputNode = audioEngine.inputNode
-            
-            recognitionRequest?.shouldReportPartialResults = true
-            
-            recognitionTask = speechRecognizer.recognitionTask(with: (recognitionRequest ?? nil)!) { result, error in
-                var isFinal = false
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("AudioSession properties weren't set because of an error.")
+        }
+        
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        let recognitionRequest = self.recognitionRequest
+        recognitionRequest?.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest!, resultHandler: {(result, error) in
+            var isFinal = false
+            if result != nil {
+                let speechedText = result?.bestTranscription.formattedString
                 
-                if let result = result {
-                    isFinal = result.isFinal
-                    
-                    self.speechResult = result
-                    self.textView.text = result.bestTranscription.formattedString
+                
+                let words = speechedText?.components(separatedBy: " ")
+                
+                for word in words! {
+                    if word.contains("Stato del treno") {
+                        print("Trenitalia")
+                    }
                 }
+                
+                self.textView.text = speechedText
+                isFinal = (result?.isFinal)!
             }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {(buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error")
+        }
+        
+        textView.text = "Say something, I'm listening!"
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            
         }
     }
 }
